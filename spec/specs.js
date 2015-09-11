aae6db8dc60aa722fdfd4c4c899899eb38efa67d
Kelli Margaret <kelli.sommerdyke@gmail.com>
2015-09-11 11:34:23 -0700

    Second test to pass.

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,7 @@ describe('findReplace', function() {
     it("will turn a string into an array of words", function() {
         expect(findReplace("Hello world", "world", "universe")).to.be.a("array");
     });
+    it("will change a specified word in the string to a new word", function() {
+        expect(findReplace("Hello world", "world", "universe")).to.eql(["Hello", "universe"]);
+    });
 });
