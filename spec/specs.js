c001bb508494430a2e2e04291a1cbd2a22412a3d
Kelli Margaret <kelli.sommerdyke@gmail.com>
2015-09-11 11:35:01 -0700

    Third test to pass.

--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,7 @@ describe('findReplace', function() {
     it("will change a specified word in the string to a new word", function() {
         expect(findReplace("Hello world", "world", "universe")).to.eql(["Hello", "universe"]);
     });
+    it("will change multiple instances of the same word to a new word", function() {
+        expect(findReplace("Hello world world", "world", "universe")).to.eql(["Hello", "universe", "universe"]);
+    });
 });
